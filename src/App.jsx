194ffe4143ecd83b47816fc721b046c7f14bead1
Tharunda De Silva <tharundadesilva@gmail.com>
2025-09-17 12:53:27 +0530

    routing setup and homepage created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import HomePage from './Pages/HomePage';
+
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<HomePage/>
+    },
+  ]);
+
   return (
     <div>
-      
+      <RouterProvider router={router}/>
     </div>
   )
 }
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores dolores accusantium itaque corrupti blanditiis in harum, aspernatur vero unde, quas obcaecati illum pariatur, animi autem dignissimos commodi vel error atque.Home</h1>
+    </div>
+  )
+}
+
+export default HomePage
