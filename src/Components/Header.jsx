a2d2c894e8daf83c78ad929c14caf8b4bc35a01e
Tharunda De Silva <tharundadesilva@gmail.com>
2025-09-18 12:56:09 +0530

    sidebsr font changed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,14 @@ const Header = () => {
     const {connectionClick,setConnectionClick,userClick,setuserClick,SecurityClick,setSecurityClick} = useAppContext();
 
   return (
-    <div className='flex fixed h-20 md:h-30 lg:h-40 w-full left-40 left:50 md:left-60 lg:left-70 bg-gray-200 lg:px-40 items-center justify-between'>
+    <div className='flex fixed h-20 md:h-30 lg:h-40 w-full left-40 left:50 md:left-60 lg:left-70 bg-gray-200 lg:px-40 items-center justify-between noto {
+  font-family: "Noto Sans Mono", monospace;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+  font-variation-settings:
+    "wdth" 100;
+}'>
         <div>
             <h1 className='text-3xl'>Connection</h1>
             <h1 className='text-xl mt-3 '>connection settings</h1>
--- src/Components/SideMenue.jsx
+++ src/Components/SideMenue.jsx
@@ -7,7 +7,7 @@ const SideMenue = () => {
     const {setConnectionClick,setuserClick,setSecurityClick}= useAppContext();
 
   return (
-    <div className='fixed flex flex-col h-screen w-40 md:w-60 top-0 lg:w-70 left-0 bg-gray-100 items-center'>
+    <div className='fixed flex flex-col h-screen w-40 md:w-60 top-0 lg:w-70 left-0 bg-gray-100 items-center noto'>
 
         <div className='flex lg:text-2xl gap-2 lg:gap-5 mt-10 lg:mt-16'>
             <i class="ri-list-settings-fill text-shadow-lg text-shadow-blue-200"></i>
@@ -15,7 +15,7 @@ const SideMenue = () => {
         </div>
 
         <div className='flex mt-10 lg:mt-25 bg-gray-300 p-3 lg:p-6 rounded-lg  lg:gap-3 lg:w-65 '>
-            <h1 className='text-xs lg:text-lg'>Application Settings</h1> 
+            <h1 className='text-xs lg:text-lg font-bold'>Application Settings</h1> 
             <i 
                 class="ri-arrow-down-double-line text-xl font-bold px-1 rounded-full bg-gray-400 cursor-pointer hover:bg-black hover:text-white"
                 onClick={()=>setDropdown(!dropdown)} 
@@ -25,26 +25,26 @@ const SideMenue = () => {
         {dropdown && <div className='flex flex-col text-sm'>
 
             <div className='flex mt-3 mx-5  bg-gray-300 p-3 lg:p-6 rounded-lg  lg:gap-3 lg:w-55 cursor-pointer hover:bg-blue-200 hover:text-blue-800'>
-                <i class="ri-signal-wifi-error-fill"></i>
+                <i class="ri-signal-wifi-error-fill text-xl"></i>
                 <h1 className=''>Connection Settings</h1> 
             </div>
 
             <div className='flex mt-3 mx-5  bg-gray-300 p-3 lg:p-6 rounded-lg  lg:gap-3 lg:w-55 cursor-pointer hover:bg-blue-200 hover:text-blue-800'>
-                <i class="ri-shield-user-fill"></i>
+                <i class="ri-shield-user-fill text-xl"></i>
                 <h1 className=''>User Management</h1> 
             </div>
 
             <div className='flex mt-3 mx-5  bg-gray-300 p-3 lg:p-6 rounded-lg  lg:gap-3 lg:w-55 cursor-pointer hover:bg-blue-200 hover:text-blue-800'>
-                <i class="ri-shield-fill"></i>
+                <i class="ri-shield-fill text-xl"></i>
                 <h1 className=''>Security Settings</h1> 
             </div>
 
         </div>}
 
         <div className='flex mt-2 lg:mt-3 bg-gray-300 p-3 lg:p-6 rounded-lg  lg:gap-3 lg:w-65'>
-            <h1 className='text-xs lg:text-lg'>General Settings</h1> 
+            <h1 className='text-xs lg:text-lg font-bold'>General Settings</h1> 
             <i 
-                class="ri-arrow-down-double-line text-xl font-bold px-1 rounded-full bg-gray-400 cursor-pointer hover:bg-black hover:text-white"
+                class="ri-arrow-down-double-line text-xl font-bold p-1 rounded-full bg-gray-400 cursor-pointer hover:bg-black hover:text-white"
                 
             ></i> 
         </div>
