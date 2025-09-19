86aa1883556993f0cb1c2432d0ba2a3199fff399
Tharunda De Silva <tharundadesilva@gmail.com>
2025-09-19 20:04:10 +0530

    api.js created

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API = axios.create(
+    {
+        baseURL: process.env.REACT_APP_API_URL,
+        headers: {
+            "Content-Type":"application/json",
+        },
+    }
+);
+
+export default API;
