77b59afc44e88de35238dc792b9eed9edac2abd4
Tharunda De Silva <tharundadesilva@gmail.com>
2025-09-22 21:57:22 +0530

    design improvements - shadows ect

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     const [showTooltip, setShowTooltip] = useState(false); 
 
   return (
-    <div className='flex relative h-20 md:h-30 lg:h-40  bg-gray-200 px-3 lg:px-40 items-center justify-between noto '>
+    <div className='flex relative h-20 md:h-30 lg:h-40  bg-gray-200 px-3 lg:px-40 items-center justify-between noto shadow-lg z-10 shadow-blue-200 '>
         {connectionClick && <div>
             <h1 className='text-sm lg:text-3xl font-bold'>Connection Settings</h1>
             <h1 className='text-xs  lg:text-xl mt-3'>Configure database, email, and SMS connections</h1>
@@ -23,23 +23,23 @@ const Header = () => {
             <h1 className='text-xs  lg:text-xl mt-3'>All security configerations</h1>
         </div>}
 
-        <div className="relative me-10">
+        <div className="relative me-10 ">
             <i 
-                className="ri-information-2-fill lg:text-3xl cursor-pointer hover:text-green-600"
+                className="ri-information-2-fill lg:text-3xl cursor-pointer hover:text-green-600 text-shadow-lg  shadow-blue-700 "
                 onMouseEnter={() => setShowTooltip(true)}
                 onMouseLeave={() => setShowTooltip(false)}
             ></i>
             
-            {showTooltip && connectionClick && <div className="absolute left-1/2 -translate-x-1/2 mt-2 w-100 z-10 px-3 py-1 rounded-lg bg-gray-700 text-white text-sm ">
-            In this section, you can configure and manage the system’s Database, Email, and SMS connections. Updating these settings ensures the application can store data, send notifications, and deliver SMS alerts reliablyThis section shows contextual information
+            {showTooltip && connectionClick && <div className="absolute p-6 left-1/2 -translate-x-1/2 mt-2 w-100 z-10  rounded-lg bg-gray-700 text-white text-sm ">
+                In this section, you can configure and manage the system’s Database, Email, and SMS connections. Updating these settings ensures the application can store data, send notifications, and deliver SMS alerts reliablyThis section shows contextual information
             </div>}
 
-            {showTooltip && userClick && <div className="absolute left-1/2 -translate-x-1/2 mt-2 w-100 z-10 px-3 py-1 rounded-lg bg-gray-700 text-white text-sm ">
-            This section allows administrators to register new users, update user details, or remove existing users to keep the system’s access up to date
+            {showTooltip && userClick && <div className="absolute left-1/2 -translate-x-1/2 mt-2 w-100 z-10 p-6 rounded-lg bg-gray-700 text-white text-sm ">
+                This section allows administrators to register new users, update user details, or remove existing users to keep the system’s access up to date
             </div>}
 
-            {showTooltip && securityClick && <div className="absolute left-1/2 -translate-x-1/2 mt-2 w-100 z-10 px-3 py-1 rounded-lg bg-gray-700 text-white text-sm ">
-            This section provides all security configurations, including access controls and system protection settings, to ensure secure operation of the application
+            {showTooltip && securityClick && <div className="absolute left-1/2 -translate-x-1/2 mt-2 w-100 z-10 p-6 rounded-lg bg-gray-700 text-white text-sm ">
+                This section provides all security configurations, including access controls and system protection settings, to ensure secure operation of the application
             </div>}
 
         </div>
--- src/Components/SMSSettings.jsx
+++ src/Components/SMSSettings.jsx
@@ -98,7 +98,7 @@ const SMSSettings = () => {
   return (
 
     <div>
-      <div className='flex relative bg-white mt-10 mx-10 lg:mx-70 p-5 border-1 border-blue-500 rounded-t-2xl justify-between text-sm lg:text-xl'>
+      <div className='flex relative bg-white mt-10 mx-10 lg:mx-70 p-5 border-1 border-blue-500 rounded-t-2xl justify-between text-sm lg:text-xl shadow-lg shadow-blue-200'>
           <div className='flex gap-3'>
             <i class="ri-message-2-fill"></i>
             <h1 >SMS Settings</h1>
